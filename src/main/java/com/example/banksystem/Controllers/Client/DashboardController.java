c8fdf59cd00d8805d75f650dae2f2d46508f66bb
karalesus <karalesus@gmail.com>
2023-12-13 00:30:41 +0300

    add account summary

--- src/main/java/com/example/banksystem/Controllers/Client/DashboardController.java
+++ src/main/java/com/example/banksystem/Controllers/Client/DashboardController.java
@@ -27,6 +27,7 @@ public class DashboardController implements Initializable {
     public TextField amount_fld;
     public TextArea message_fld;
     public Button send_money_btn;
+    public Label info_lbl;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -35,11 +36,12 @@ public class DashboardController implements Initializable {
         transaction_listview.setItems(Model.getInstance().getLatestTransactions());
         transaction_listview.setCellFactory(e -> new TransactionCellFactory());
         send_money_btn.setOnAction(event -> onSendMoney());
+        accountSummary();
     }
 
     private void bindData() {
         user_name.textProperty().bind(Bindings.concat("Привет, ").concat(Model.getInstance().getClient().firstNameProperty()));
-        login_date.setText("Сегодня "+ LocalDate.now());
+        login_date.setText("Сегодня " + LocalDate.now());
         checking_bal.textProperty().bind(Model.getInstance().getClient().checkingAccountProperty().get().balanceProperty().asString());
         checking_acc_num.textProperty().bind(Model.getInstance().getClient().checkingAccountProperty().get().accountNumberProperty());
         saving_bal.textProperty().bind(Model.getInstance().getClient().savingsAccountProperty().get().balanceProperty().asString());
@@ -48,7 +50,7 @@ public class DashboardController implements Initializable {
 
     // инициализириуем лист транзакций один раз, чтобы каждый раз не делать это по новой
     private void initTransactionsList() {
-        if (Model.getInstance().getLatestTransactions().isEmpty()){
+        if (Model.getInstance().getLatestTransactions().isEmpty()) {
             Model.getInstance().setLatestTransactions();
         }
     }
@@ -60,11 +62,12 @@ public class DashboardController implements Initializable {
         String sender = Model.getInstance().getClient().pAddressProperty().get();
         ResultSet resultSet = Model.getInstance().getDatabaseDriver().searchClient(receiver);
         try {
-            if (resultSet.isBeforeFirst()){
-                Model.getInstance().getDatabaseDriver().updateBalance(receiver,amount,"ADD");
-
+            if (resultSet.isBeforeFirst()) {
+                Model.getInstance().getDatabaseDriver().updateBalance(receiver, amount, "ADD");
+                info_lbl.setStyle("-fx-text-fill: blue; -fx-font-size: 1.0em; -fx-font-weight: bold");
+                info_lbl.setText("Деньги успешно отправлены!");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         // уменьшение баланса вклада отправителя
@@ -75,5 +78,27 @@ public class DashboardController implements Initializable {
         payee_fld.setText("");
         amount_fld.setText("");
         message_fld.setText("");
+    }
+
+    // Метод, который считает доходы и расходы
+    private void accountSummary() {
+        double income = 0;
+        double expenses = 0;
+        // инициализируем ВСЕ транзакции
+        if (Model.getInstance().getAllTransactions().isEmpty()) {
+            Model.getInstance().setAllTransactions();
+        }
+        // в листе транзакций смотрим,
+        // кто отправитель денег. если это тот, кто сейчас в аккаунте,
+        // то мы считаем это за расход (иначе - доход)
+        for (Transaction transaction : Model.getInstance().getAllTransactions()) {
+            if (transaction.senderProperty().get().equals(Model.getInstance().getClient().pAddressProperty().get())) {
+                expenses = expenses + transaction.amountProperty().get();
+            } else {
+                income = income + transaction.amountProperty().get();
+            }
+        }
+        income_lbl.setText("+ " + income + " ₽");
+        expense_lbl.setText("- " + expenses + " ₽");
     }
 }
